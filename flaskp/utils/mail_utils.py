eef3260578f3eececd66eb24e21da5791d679fa0
liyongjin <yongjinaple@aliyun.com>
2022-05-13 08:52:49 +0800

    添加邮配置注释

--- flaskp/utils/mail_utils.py
+++ flaskp/utils/mail_utils.py
@@ -12,6 +12,17 @@ from flaskp.extensions import mail
 
 
 def send_mail():
+    """
+    邮件配置
+    MAIL_SERVER = 'smtp.yeah.net'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'your email'
+    MAIL_PASSWORD = 'your email password'
+    MAIL_DEFAULT_SENDER = 'your email'
+    MAIL_DEBUG = 0  # 0--关闭调试 1--开启调试
+    """
     msg = Message("Hello",
                   sender=config.MAIL_USERNAME,
                   recipients=["lyjorange2019@126.com"])
